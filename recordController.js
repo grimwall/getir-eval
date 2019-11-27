e48b8f135bbdd8438c78f0121f484807cf2a1055
ahmet_apaydin <ahmeta@gmail.com>
2019-11-28 00:00:10 +0300

    gitignore fixed for vscode

--- recordController.js
+++ recordController.js
@@ -23,6 +23,19 @@ exports.index = function (req, res) {
 
 //searches for records
 exports.search = function (req, res) {
+ 
+    var errorMessage = hasValidInputs(req);
+
+    if (errorMessage){
+        res.json({
+            code: 1,
+            msg: errorMessage
+        });
+        return res;
+    }
+
+    var startDate = moment(req.body.startDate);
+    var endDate = moment(req.body.endDate);
     //todo figure out a better way for filtering on array sum
     var whereFunc = `function () {  const arrSum = arr => arr.reduce((a, b) => a + b, 0);
                 var sum = arrSum(this.counts); 
@@ -30,8 +43,8 @@ exports.search = function (req, res) {
 
     var query = Record
         .where('createdAt')
-        .gte(new Date(req.body.startDate))
-        .lte(new Date(req.body.endDate))
+        .gte(new Date(startDate.toDate()))
+        .lte(new Date(endDate.toDate()))
         .$where(whereFunc)
         //.select('createdAt key counts -_id')
         ;
@@ -50,9 +63,24 @@ exports.search = function (req, res) {
                 rec.key = r.key;
                 rec.createdAt = r.createdAt;
                 rec.totalCount = arrSum(r.counts);
-                
+
                 return rec;
             })
         });
     });
+};
+
+const hasValidInputs = function(req, err){
+    //validate input
+    var startDate = moment(req.body.startDate);
+    var endDate = moment(req.body.endDate);    
+    
+    if (!startDate.isValid() || !endDate.isValid()) {
+        console.log('invalid dates!');
+        return 'invalid dates!';
+    }
+
+    if (isNaN(req.body.minCount )  || isNaN(req.body.maxCount) ){
+        return 'invalid counts!';
+    }
 };
