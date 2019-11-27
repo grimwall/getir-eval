0278a5d749e17b01becdc36c480e9ccdd3e1322e
ahmet_apaydin <ahmeta@gmail.com>
2019-11-28 00:13:50 +0300

    cleanup recordController

--- recordController.js
+++ recordController.js
@@ -2,7 +2,7 @@ const moment = require('moment');
 
 Record = require('./recordModel');
 
-const arrSum = arr => arr.reduce((a, b) => a + b, 0);
+
 
 //index actions
 exports.index = function (req, res) {
@@ -23,10 +23,10 @@ exports.index = function (req, res) {
 
 //searches for records
 exports.search = function (req, res) {
- 
+
     var errorMessage = hasValidInputs(req);
 
-    if (errorMessage){
+    if (errorMessage) {
         res.json({
             code: 1,
             msg: errorMessage
@@ -36,17 +36,19 @@ exports.search = function (req, res) {
 
     var startDate = moment(req.body.startDate);
     var endDate = moment(req.body.endDate);
+
     //todo figure out a better way for filtering on array sum
+    //predicate to filter only records with sum of counts betwen required counts
     var whereFunc = `function () {  const arrSum = arr => arr.reduce((a, b) => a + b, 0);
                 var sum = arrSum(this.counts); 
                 return (sum >= `+ req.body.minCount + ` && sum <= ` + req.body.maxCount + `);    }`;
+
 
     var query = Record
         .where('createdAt')
         .gte(new Date(startDate.toDate()))
         .lte(new Date(endDate.toDate()))
         .$where(whereFunc)
-        //.select('createdAt key counts -_id')
         ;
 
     query.exec(function (err, records) {
@@ -59,10 +61,11 @@ exports.search = function (req, res) {
             code: 0,
             msg: 'Retrieved record count: ' + records.length,
             records: records.map(r => {
+                //return only the required fields
                 var rec = {};
                 rec.key = r.key;
                 rec.createdAt = r.createdAt;
-                rec.totalCount = arrSum(r.counts);
+                rec.totalCount = arraySum(r.counts);
 
                 return rec;
             })
@@ -70,17 +73,21 @@ exports.search = function (req, res) {
     });
 };
 
-const hasValidInputs = function(req, err){
+//sums a number array
+const arraySum = arr => arr.reduce((a, b) => a + b, 0);
+
+//validates dates and counts
+const hasValidInputs = function (req) {
     //validate input
     var startDate = moment(req.body.startDate);
-    var endDate = moment(req.body.endDate);    
-    
+    var endDate = moment(req.body.endDate);
+
     if (!startDate.isValid() || !endDate.isValid()) {
         console.log('invalid dates!');
         return 'invalid dates!';
     }
 
-    if (isNaN(req.body.minCount )  || isNaN(req.body.maxCount) ){
+    if (isNaN(req.body.minCount) || isNaN(req.body.maxCount)) {
         return 'invalid counts!';
     }
 };
